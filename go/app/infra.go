2473c7ecb03834b3891855c367200d0415424262
mii0829 <kanokanosakusaku@gmail.com>
2025-03-07 10:26:40 +0900

    step5-1 error

--- go/app/infra.go
+++ go/app/infra.go
@@ -2,11 +2,13 @@ package app
 
 import (
 	"context"
-	"encoding/json"
+	"database/sql"
 	"errors"
-	"os"
+	"log"
+	"sync"
+
 	// STEP 5-1: uncomment this line
-	// _ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var errImageNotFound = errors.New("image not found")
@@ -25,6 +27,7 @@ type Item struct {
 type ItemRepository interface {
 	Insert(ctx context.Context, item *Item) error
 	GetAll(ctx context.Context) ([]*Item, error)
+	GetByID(ctx context.Context, id int) (*Item, error)
 }
 
 // itemRepository is an implementation of ItemRepository
@@ -38,65 +41,95 @@ func NewItemRepository() ItemRepository {
 	return &itemRepository{fileName: "items.json"}
 }
 
+var (
+	db   *sql.DB
+	once sync.Once
+)
+
+// DBを読み込む
+func getDB() *sql.DB {
+	once.Do(func() { //１回だけDBを開く
+		var err error
+		db, err = sql.Open("sqlite3", "db/mercari.sqlite3")
+		if err != nil {
+			log.Fatalf("failed to connect to database")
+		} else {
+			log.Printf("success to connect to database")
+		}
+	})
+	return db
+}
+
 // Insert inserts an item into the repository.
 func (i *itemRepository) Insert(ctx context.Context, item *Item) error {
-	// STEP 4-1: add an implementation to store an item
+	db := getDB()
 
-	items, err := i.loadItems()
+	query := "INSERT INTO items (name, category, image_name) VALUES (?, ?, ?)"
+	_, err := db.ExecContext(ctx, query, item.Name, item.Category, item.Image)
 
 	if err != nil {
 		return err
 	}
 
-	items = append(items, *item)
-
-	return i.saveItems(items)
+	return err
 }
 
 // GetAll：items.jsonから全商品を取得
 func (i *itemRepository) GetAll(ctx context.Context) ([]*Item, error) {
-	items, err := i.loadItems()
+	db := getDB()
+
+	rows, err := db.QueryContext(ctx, "SELECT id, name, category, image_name FROM items")
+
 	if err != nil {
 		return nil, err
 	}
 
-	var itemPtrs []*Item
-	for index := range items {
-		itemPtrs = append(itemPtrs, &items[index])
-	}
+	defer rows.Close()
 
-	return itemPtrs, nil
-}
-
-// items.jsonを読み込み
-func (i *itemRepository) loadItems() ([]Item, error) {
-	file, err := os.OpenFile(i.fileName, os.O_RDWR|os.O_CREATE, 0644)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var items []Item
-	err = json.NewDecoder(file).Decode(&items)
-	if err != nil && err.Error() != "EOF" {
-		return nil, err
+	var items []*Item
+	for rows.Next() {
+		var item Item
+		err := rows.Scan(&item.ID, &item.Name, &item.Category, &item.Image)
+		if err != nil {
+			return nil, err
+		}
+		items = append(items, &item)
 	}
 
 	return items, nil
 }
 
-// items.jsonに保存
-func (i *itemRepository) saveItems(items []Item) error {
-	file, err := os.Create(i.fileName)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
+// IDから特定の商品を取得
+func (r *itemRepository) GetByID(ctx context.Context, id int) (*Item, error) {
+	db := getDB()
 
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
-	return encoder.Encode(items)
+	row := db.QueryRowContext(ctx, "SELECT id, name, category, image_name FROM items WHERE id = ?", id)
+
+	var item Item
+	err := row.Scan(&item.ID, &item.Name, &item.Category, &item.Image)
+	if err != nil {
+		return nil, err
+	}
+
+	return &item, nil
 }
+
+// items.jsonを読み込み
+// func (i *itemRepository) loadItems() ([]Item, error) {
+// 	file, err := os.OpenFile(i.fileName, os.O_RDWR|os.O_CREATE, 0644)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer file.Close()
+
+// 	var items []Item
+// 	err = json.NewDecoder(file).Decode(&items)
+// 	if err != nil && err.Error() != "EOF" {
+// 		return nil, err
+// 	}
+
+// 	return items, nil
+// }
 
 // StoreImage stores an image and returns an error if any.
 // This package doesn't have a related interface for simplicity.
